a0a7a5fb0730dee722e26c726f46f2a941b77258
Dawan Sawyer <dawansawyer@gmail.com>
2024-05-17 19:47:59 -0400

    refactor: Improve Square component to render chess pieces Update Square component to accept a piece prop for rendering chess pieces Add conditional rendering logic to display chess pieces based on the type of piece Enhance modularity and reusability by extracting rendering logic into a separate function

--- chess-game/src/components/Square.jsx
+++ chess-game/src/components/Square.jsx
@@ -1,9 +1,20 @@
-const Square = ({color}) => {
+import Queen from "./pieces/Queen";
+
+const Square = ({color, piece, onClick, highlight}) => {
     return (
-        <div className={`square ${color}`}>
-            
+        <div className={`square ${color} ${highlight}`} onClick={onClick}>
+            {piece && renderPiece(piece)}
         </div>
     );
+};
+
+const renderPiece = (piece) => {
+    switch (piece.type) {
+        case 'queen' :
+            return <Queen color={piece.color} />;
+        default:
+            return null;
+    }
 };
 
 export default Square;
