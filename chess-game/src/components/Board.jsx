ac88cd335db7d04d9677d671ca8421b7941a6815
Dawan Sawyer <dawansawyer@gmail.com>
2024-05-18 12:54:22 -0400

    feat: Refactor Board component to include gameStarted prop and useEffect hook Updated Board component to accept gameStarted prop for conditional rendering of squares Implemented useEffect hook to initialize board state when playerColor prop changes Adjusted rendering logic to pass gameStarted and playerColor props to child Square components
    This refactor enhances the Board component by improving modularity and introducing dynamic initialization based on game state.

--- chess-game/src/components/Board.jsx
+++ chess-game/src/components/Board.jsx
@@ -1,63 +1,74 @@
-import { useState } from 'react';
-import Square from './Square'
-import { initializeBoardState } from '../utils/utils';
-import { getValidMoves } from '../utils/chessRules';
+import { useEffect, useState } from "react";
+import "../styles/board.css";
+import Square from "./Square";
+import { initializeBoardState } from "../utils/utils";
+import { getValidMoves } from "../utils/chessRules";
 
-const Board = ({ playerColor }) => {
-    const [boardState, setBoardState] = useState(() => initializeBoardState(playerColor)); 
-    const [selectedSquare, setSelectedSquare] = useState(null)
-    const [validMoves, setValidMoves] = useState([]);
+const Board = ({ playerColor, gameStarted }) => {
+  const [boardState, setBoardState] = useState(() =>
+    initializeBoardState(playerColor)
+  );
+  const [selectedSquare, setSelectedSquare] = useState(null);
+  const [validMoves, setValidMoves] = useState([]);
 
-    const handleSquareClick = (position) => {
-        if (selectedSquare) {
-          if (validMoves.includes(position)) {
-            const updatedBoardState = { ...boardState };
-            updatedBoardState[position] = updatedBoardState[selectedSquare];
-            delete updatedBoardState[selectedSquare];
-            setBoardState(updatedBoardState);
-            setSelectedSquare(null);
-            setValidMoves([]);
-          } else {
-            setSelectedSquare(null);
-            setValidMoves([])
-          }
-        } else {
-            if (boardState[position]) {
-                const piece = boardState[position];
-                setSelectedSquare(position);
-                setValidMoves(getValidMoves(piece, position, boardState));
-            }
-        }
-    };
+  useEffect(() => {
+    if (playerColor) {
+      setBoardState(initializeBoardState(playerColor));
+    }
+  }, [playerColor]);
 
-    const renderBoard = () => {
-        const board = [];
-        for (let row = 1; row <= 8; row++) {
-            for (let col = 0; col < 8; col++) {
-                const position = `${String.fromCharCode(97 + col)}${row}`;
-                const piece = boardState[position];
-                const color = (row + col) % 2 === 0 ? 'light' : 'dark';
-                const highlight = validMoves.includes(position) ? 'highlight' : '';
-                board.push(
-                    <Square
-                        key={position}
-                        position={position}
-                        color={color}
-                        piece={piece}
-                        highlight={highlight}
-                        onClick={handleSquareClick(position)} 
-                    />
-                );
-            }
-        }
-        return board;
-    };
+  const handleSquareClick = (position) => {
+    if (selectedSquare) {
+      if (validMoves.includes(position)) {
+        const updatedBoardState = { ...boardState };
+        updatedBoardState[position] = updatedBoardState[selectedSquare];
+        delete updatedBoardState[selectedSquare];
+        setBoardState(updatedBoardState);
+        setSelectedSquare(null);
+        setValidMoves([]);
+      } else {
+        setSelectedSquare(null);
+        setValidMoves([]);
+      }
+    } else {
+      if (boardState[position]) {
+        const piece = boardState[position];
+        setSelectedSquare(position);
+        setValidMoves(getValidMoves(piece, position, boardState));
+      }
+    }
+  };
 
-    return (
-        <div className="board">
-            {renderBoard()}
-        </div>
-    );
+  const renderBoard = () => {
+    const board = [];
+    for (let row = 1; row <= 8; row++) {
+      for (let col = 0; col < 8; col++) {
+        const position = `${String.fromCharCode(97 + col)}${row}`;
+        const piece = boardState[position];
+        const color = (row + col) % 2 === 0 ? "light" : "dark";
+        const highlight = validMoves.includes(position) ? "highlight" : "";
+        board.push(
+          <Square
+            key={position}
+            position={position}
+            color={color}
+            piece={piece}
+            highlight={highlight}
+            gameStarted={gameStarted}
+            playerColor={playerColor}
+            onClick={() => handleSquareClick(position)}
+          />
+        );
+      }
+    }
+    return board;
+  };
+
+  return (
+    <div className="container">
+      <div className="board">{renderBoard()}</div>
+    </div>
+  );
 };
 
 export default Board;
